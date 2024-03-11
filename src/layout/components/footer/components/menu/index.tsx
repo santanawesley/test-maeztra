855a5f3e4fc0e606f3aeaf2f303b180dfc99c33f
Wesley Santana <wesley.santana@opah.com.br>
2024-03-11 00:56:20 -0300

    Adapting the footer menu for mobile

--- src/layout/components/footer/components/menu/index.tsx
+++ src/layout/components/footer/components/menu/index.tsx
@@ -1,83 +1,118 @@
+import { useState } from "react";
 import "./menu-footer.scss";
 
 const MenuFooter = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+  const [selectedCategory, setSelectedCategory] = useState(0);
+
+  const handleCategoryClick = (category: number) => {
+    if (menuOpen && category === selectedCategory) {
+      setMenuOpen(false);
+      setSelectedCategory(0);
+    } else {
+      setMenuOpen(true);
+      setSelectedCategory(category);
+    }
+  };
+
   return (
     <div className="wrapper-menu-footer">
       <div className="menu-footer">
         <div className="columns-menu-footer category">
           <div className="wrapper-title-menu-footer">
             <h3 className="title-menu-footer">Informações</h3>
-            <button className="toggle-btn">+</button>
+            <button
+              className="toggle-btn"
+              onClick={() => handleCategoryClick(1)}
+            >
+              {menuOpen && selectedCategory === 1 ? "-" : "+"}
+            </button>
           </div>
-          <nav className="links">
-            <ul>
-              <li>
-                <a href="#" className="item-menu-footer">
-                  Quem Somos
-                </a>
-              </li>
-              <li>
-                <a href="#" className="item-menu-footer">
-                  Prazo de Envio
-                </a>
-              </li>
-              <li>
-                <a href="#" className="item-menu-footer">
-                  Trocas e Devoluções
-                </a>
-              </li>
-              <li>
-                <a href="#" className="item-menu-footer">
-                  Promoções e Cupons
-                </a>
-              </li>
-            </ul>
-          </nav>
+          {menuOpen && selectedCategory === 1 && (
+            <nav className="links">
+              <ul>
+                <li>
+                  <a href="#" className="item-menu-footer">
+                    Quem Somos
+                  </a>
+                </li>
+                <li>
+                  <a href="#" className="item-menu-footer">
+                    Prazo de Envio
+                  </a>
+                </li>
+                <li>
+                  <a href="#" className="item-menu-footer">
+                    Trocas e Devoluções
+                  </a>
+                </li>
+                <li>
+                  <a href="#" className="item-menu-footer">
+                    Promoções e Cupons
+                  </a>
+                </li>
+              </ul>
+            </nav>
+          )}
         </div>
         <div className="columns-menu-footer category">
           <div className="wrapper-title-menu-footer">
             <h3 className="title-menu-footer">Minha Conta</h3>
-            <button className="toggle-btn">+</button>
+            <button
+              className="toggle-btn"
+              onClick={() => handleCategoryClick(2)}
+            >
+              {menuOpen && selectedCategory === 2 ? "-" : "+"}
+            </button>
           </div>
-          <nav className="links">
-            <ul>
-              <li>
-                <a href="#" className="item-menu-footer">
-                  Minha Conta
-                </a>
-              </li>
-              <li>
-                <a href="#" className="item-menu-footer">
-                  Meus Pedidos
-                </a>
-              </li>
-              <li>
-                <a href="#" className="item-menu-footer">
-                  Cadastre-se
-                </a>
-              </li>
-            </ul>
-          </nav>
+          {menuOpen && selectedCategory === 2 && (
+            <nav className="links">
+              <ul>
+                <li>
+                  <a href="#" className="item-menu-footer">
+                    Minha Conta
+                  </a>
+                </li>
+                <li>
+                  <a href="#" className="item-menu-footer">
+                    Meus Pedidos
+                  </a>
+                </li>
+                <li>
+                  <a href="#" className="item-menu-footer">
+                    Cadastre-se
+                  </a>
+                </li>
+              </ul>
+            </nav>
+          )}
         </div>
         <div className="columns-menu-footer category">
           <div className="wrapper-title-menu-footer">
             <h3 className="title-menu-footer">Onde nos Encontrar</h3>
-            <button className="toggle-btn">+</button>
+            <button
+              className="toggle-btn"
+              onClick={() => handleCategoryClick(3)}
+            >
+              {menuOpen && selectedCategory === 3 ? "-" : "+"}
+            </button>
           </div>
-          <nav className="links">
-            <ul>
-              <li>
-                <a href="#" className="item-menu-footer">
-                  Lojas
-                </a>
-              </li>
-              <li>
-                <a href="#" className="item-menu-footer">
-                  Endereço
-                </a>
-              </li>
-            </ul>
-          </nav>
+          {menuOpen && selectedCategory === 3 && (
+            <nav className="links">
+              <ul>
+                <li>
+                  <a href="#" className="item-menu-footer">
+                    Lojas
+                  </a>
+                </li>
+                <li>
+                  <a href="#" className="item-menu-footer">
+                    Endereço
+                  </a>
+                </li>
+              </ul>
+            </nav>
+          )}
         </div>
       </div>
     </div>
